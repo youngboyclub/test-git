33ed4e38e8c15a2e7578c184637c0beb89ced176
RSH <fbtnghks7906@gmail.com>
2023-11-01 14:33:52 +0900

    회원가입 버튼 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import JoinButton from './components/RSH';
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+  return  <JoinButton />;
+};
