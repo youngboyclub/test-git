ad0bf5136201b0164efbf9349bba87c8daab945b
danbab <danbab2@naver.com>
2023-11-01 14:11:51 +0900

    mdc

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import What from "./components/Mdc";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <What />
+    </>
+  );
 }
---
+++ src/components/Mdc.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button className="drop-shadow-lg bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
+        클릭하세요
+      </button>
+    </div>
+  );
+};
+
+export default App;
