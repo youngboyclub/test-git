90c376f0721e3add9be886f86a4b812b2e9e155e
thkim610 <thkim610@naver.com>
2023-11-01 14:27:02 +0900

    컴포넌트 (버튼)생성

---
+++ src/components/Kth.js
@@ -0,0 +1,8 @@
+const MyButton = ()=>{
+    let style = "bg-[#365347] text-xs rounded-lg w-20 h-8 shadow-[0_4px_4px_0px_rgba(0,0,0,0.25)] font-['inter'] text-[#FAFAFA] shrink-0"
+    return (
+      <button className={style}>로그인</button>
+    );
+  }
+
+  export {MyButton};
